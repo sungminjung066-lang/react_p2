3d7c4ade07071948d8dcc7e03c9c93351f643857
sungmin jung <sungminjung066@gmail.com>
2026-01-12 10:10:54 +0900

    프리티어 추가

---
+++ my-react-app/prettierrc.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  jsxSingleQuote: false,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: 'always',
+  // plugins: ['@trivago/prettier-plugin-sort-imports', 'prettier-plugin-tailwindcss'],
+  // importOrder: ['^react$|^react-dom', '<THIRD_PARTY_MODULES>', '^@/(.*)$', '^[./]'],
+  // importOrderSeparation: true,
+  // importOrderSortSpecifiers: true,
+  endOfLine: 'auto',
+};
