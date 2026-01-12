6ecbaf3c3210757313bc63f68bdcbf217a0fa686
sungmin jung <sungminjung066@gmail.com>
2026-01-12 15:49:18 +0900

    My age 작성

--- my-react-app/src/MyAge.tsx
+++ my-react-app/src/MyAge.tsx
@@ -1,3 +1,11 @@
-export function MyAge({ age }: { age: number }) {
+// export function MyAge({ age }: { age: number }) {
+//return <div className="text-2xl">My Age: {age}</div>;
+// }
+
+import React from 'react';
+import { MyContext } from './context';
+
+export function MyAge() {
+  const { age } = React.useContext(MyContext);
   return <div className="text-2xl">My Age: {age}</div>;
 }
